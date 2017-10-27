2ccaaa798882b5e89d79cbac711b544ad294c2e4
Till Schulte-Coerne <till.schulte-coerne@innoq.com>
2017-10-27 08:54:05 +0200

    Remove default exports

--- app/views/manifest.js
+++ app/views/manifest.js
@@ -1,4 +1,4 @@
 // This has to list every single view to be used
-export {default as DemoIndex} from './demo/index.jsx';
-export {default as DemoStreaming} from './demo/streaming.jsx';
-export {default as DemoError} from './demo/error.jsx';
+export * from './demo/index.jsx';
+export * from './demo/streaming.jsx';
+export * from './demo/error.jsx';
