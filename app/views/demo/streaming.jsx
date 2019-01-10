cf6e958269b31a04665095004c317fcb8f9a90de
Till Schulte-Coerne <till.schulte-coerne@innoq.com>
2019-01-10 11:30:07 +0100

    Streaming does still not work...
    ... but at least it might ...
    
    I don't get why it doesn't

--- app/views/demo/streaming.jsx
+++ app/views/demo/streaming.jsx
@@ -2,14 +2,14 @@ import { createElement } from "complate-stream";
 import ApplicationLayout from "../layouts/application.jsx";
 
 export default ({ sleep }) => {
-	return <ApplicationLayout title="Streaming Demo Page">
+  return <ApplicationLayout title="Streaming Demo Page">
     <h1>Some Streaming for you</h1>
-		<p>Hope you're using puma...</p>
-		{() => sleep()}
-		<p>We won't let you wait for too long.</p>
-		{() => sleep()}
-		<p>But let's see what comes next...</p>
-		{() => sleep()}
-		<p>We're done.</p>
+    <p>Hope you're using puma...</p>
+    {(cb) => { sleep(); cb(<span>.</span>)}}
+    <p>We won't let you wait for too long.</p>
+    {(cb) => { sleep(); cb(<span>.</span>) }}
+    <p>But let's see what comes next...</p>
+    {(cb) => { sleep(); cb(<span>.</span>) }}
+    <p>We're done.</p>
   </ApplicationLayout>
 };
